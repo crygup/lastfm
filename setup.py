175d5165c3c6d495a7a814fd806acaa31a1b33db
curiositly <curiositlyxd@gmail.com>
2022-10-02 21:10:55 -0800

    First time setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="lastfm",
+    version="1.0.0",
+    description="python last.fm api wrapper",
+    packages=["lastfm"],
+    python_requires=">=3.8",
+    install_requires=requirements,
+)
