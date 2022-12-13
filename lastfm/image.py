ab335e43a6cc1dc62968d0cf4d999d172a7474d1
curiositly <curiositlyxd@gmail.com>
2022-12-12 19:04:59 -0900

    Make all functions in image property

--- lastfm/image.py
+++ lastfm/image.py
@@ -7,36 +7,42 @@ class Image:
     def __init__(self, data: Dict[Any, Any]) -> None:
         self._data = data
 
+    @property
     def small(self) -> Optional[str]:
         try:
             return self._data[0]["#text"]
         except (KeyError, IndexError):
             return None
 
+    @property
     def medium(self) -> Optional[str]:
         try:
             return self._data[1]["#text"]
         except (KeyError, IndexError):
             return None
 
+    @property
     def large(self) -> Optional[str]:
         try:
             return self._data[2]["#text"]
         except (KeyError, IndexError):
             return None
 
+    @property
     def extralarge(self) -> Optional[str]:
         try:
             return self._data[3]["#text"]
         except (KeyError, IndexError):
             return None
 
+    @property
     def mega(self) -> Optional[str]:
         try:
             return self._data[4]["#text"]
         except (KeyError, IndexError):
             return None
 
+    @property
     def extra_mega(self) -> Optional[str]:
         """Not sure what to call this as last.fm doesn't give a proper name it's just empty and is rarely used."""
         try:
