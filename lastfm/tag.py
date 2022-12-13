f3730cf560431afb4b297aacd9458f4673a28321
curiositly <curiositlyxd@gmail.com>
2022-12-12 19:13:30 -0900

    Add tag class

---
+++ lastfm/tag.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Optional
+
+__all__ = ["Tag"]
+
+
+class Tag:
+    def __init__(self, data: Dict[Any, Any]) -> None:
+        self._data = data
+
+    @property
+    def name(self) -> str:
+        return self._data["name"]
+
+    @property
+    def url(self) -> str:
+        return self._data["url"]
+
+    @property
+    def count(self) -> Optional[int]:
+        """This is only present with User.get_tags()"""
+        return int(self._data["count"])
