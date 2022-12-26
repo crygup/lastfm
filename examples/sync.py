1446634c4311065bcabb772d5b9e1b7045f427e3
curiositly <curiositlyxd@gmail.com>
2022-12-26 13:59:28 -0900

    Remove sync (for now)

--- examples/sync.py
+++
@@ -1,8 +0,0 @@
-import lastfm
-
-
-client = lastfm.SyncClient("api_key_here")
-
-user = client.fetch_user("crygup")
-
-print(user.playcount)
---
+++ lastfm/errors.py
@@ -0,0 +1,15 @@
+class BaseException(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class InvalidArguments(BaseException):
+    pass
+
+
+class NotFound(BaseException):
+    pass
